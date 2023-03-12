72d05c2c3c93666ccc8e446a6d59cf946123b6c5
Scott Gilbert <scottvtgilbert@gmail.com>
2023-03-12 12:29:02 -0400

    Updating map for geolocating at users current position

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,24 +1,74 @@
 //Libraries
 import { GoogleMap, useJsApiLoader, KmlLayer } from "@react-google-maps/api";
 import { useCallback, useState } from "react";
+//Styles
+import map_styles from '@/styles/map_page_styles.module.scss'
 
 export async function getServerSideProps() {
     return ({ props: { apiKey: process.env.GOOGLE_MAPS_API } })
 }
 
 export default function Woods_Map({ apiKey }) {
+    const [map, setMap] = useState(null);
+    const [infoWindow, setInfoWindow] = useState(null)
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: apiKey
     })
 
-    const [map, setMap] = useState(null);
+    function locate() {
+        map.setZoom(19);
+        navigator.geolocation.getCurrentPosition(initialLocate, locateError, options)
+        navigator.geolocation.watchPosition(locateSuccess, locateError, options)
+    }
+
+    function initialLocate(position) {
+        map.setZoom(18);
+        const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        }
+        map.setCenter(pos);
+        infoWindow.setContent("<img src='https://bearcobble.com/Assets/Images/bearpaw.png' width='17px'>");
+    }
+
+    function locateSuccess(position) {
+        const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        infoWindow.setPosition(pos);
+        infoWindow.open(map);
+        console.log(position.coords.accuracy);
+    };
+
+    function locateError() {
+        alert("Error: Make sure location settings are on and the site is entered using https - https://bearcobble.com/map.");
+    };
+
+    const options = {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000
+    };
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(
+            browserHasGeolocation
+                ? "Error: Make sure location settings are on and the site is entered using https - https://bearcobble.com/map."
+                : "Error: Your browser doesn't support geolocation."
+        );
+        infoWindow.open(map);
+    }
+
 
     const onLoad = useCallback(function callback(map) {
         const bounds = new window.google.maps.LatLngBounds(
             new window.google.maps.LatLng(44.184285, -72.961111),
             new window.google.maps.LatLng(44.163204, -72.942872))
         map.fitBounds(bounds);
+        setInfoWindow(new window.google.maps.InfoWindow())
         setMap(map)
     }, [])
 
@@ -28,52 +78,55 @@ export default function Woods_Map({ apiKey }) {
 
 
     return isLoaded ? (
-        <GoogleMap
-            mapContainerStyle={{ height: '95vh', width: '100%' }}
-            center={{ lat: 44.172887, lng: -72.956834 }}
-            zoom={15}
-            onLoad={onLoad}
-            onUnmount={onUnmount}
-            options={{
-                mapTypeId: 'terrain',
-                mapTypeControl: true,
-                controlSize: 30
-            }}
-        >
-            <KmlLayer
-                url='http://bearcobble.com/KML/ConductorsA.kml'
-                options={{ preserveViewport: true }}
-            />
-            <KmlLayer
-                url='http://bearcobble.com/KML/Roads4.kml'
-                options={{ preserveViewport: true }}
-            />
-            <KmlLayer
-                url='http://bearcobble.com/KML/Driveway.kml'
-                options={{ preserveViewport: true }}
-            />
-            <KmlLayer
-                url='http://bearcobble.com/KML/Section1.kml'
-                options={{ preserveViewport: true }}
-            />
-            <KmlLayer
-                url='http://bearcobble.com/KML/Section2a.kml'
-                options={{ preserveViewport: true }}
-            />
-            <KmlLayer
-                url='http://bearcobble.com/KML/Section3a.kml'
-                options={{ preserveViewport: true }}
-            />
-            <KmlLayer
-                url='http://bearcobble.com/KML/Section4.kml'
-                options={{ preserveViewport: true }}
-            />
-            <KmlLayer
-                url='http://bearcobble.com/KML/Section5.kml'
-                options={{ preserveViewport: true }}
-            />
+        <div className={map_styles.map_container}>
 
-        </GoogleMap>
+            <GoogleMap
+                mapContainerStyle={{ height: '95vh', width: '100%' }}
+                center={{ lat: 44.172887, lng: -72.956834 }}
+                zoom={15}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+                options={{
+                    mapTypeId: 'terrain',
+                    mapTypeControl: true,
+                    controlSize: 30
+                }}
+            >
+                <KmlLayer
+                    url='http://bearcobble.com/KML/ConductorsA.kml'
+                    options={{ preserveViewport: true }}
+                />
+                <KmlLayer
+                    url='http://bearcobble.com/KML/Roads4.kml'
+                    options={{ preserveViewport: true }}
+                />
+                <KmlLayer
+                    url='http://bearcobble.com/KML/Driveway.kml'
+                    options={{ preserveViewport: true }}
+                />
+                <KmlLayer
+                    url='http://bearcobble.com/KML/Section1.kml'
+                    options={{ preserveViewport: true }}
+                />
+                <KmlLayer
+                    url='http://bearcobble.com/KML/Section2a.kml'
+                    options={{ preserveViewport: true }}
+                />
+                <KmlLayer
+                    url='http://bearcobble.com/KML/Section3a.kml'
+                    options={{ preserveViewport: true }}
+                />
+                <KmlLayer
+                    url='http://bearcobble.com/KML/Section4.kml'
+                    options={{ preserveViewport: true }}
+                />
+                <KmlLayer
+                    url='http://bearcobble.com/KML/Section5.kml'
+                    options={{ preserveViewport: true }}
+                />
+                <button className={map_styles.zoom_to_me} onClick={locate}>Zoom To Me</button>
+            </GoogleMap>
+        </div>
     ) : <></>
 
 }
