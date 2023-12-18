a0add31d147834957c698126d3b2d19133b29705
Scott Gilbert <scottvtgilbert@gmail.com>
2023-12-17 21:29:59 -0500

    Updating times for daylight savings switch

--- src/utils/adjForUTCDate.js
+++ src/utils/adjForUTCDate.js
@@ -2,7 +2,7 @@
 export function adjustForUTC(timeToConvert){
     let currentHours = timeToConvert.getUTCHours();
     // let hourDiff = 0;
-    let hourDiff = 4;
+    let hourDiff = 5;
     timeToConvert.setUTCHours(currentHours-hourDiff);
     return timeToConvert;
 }
