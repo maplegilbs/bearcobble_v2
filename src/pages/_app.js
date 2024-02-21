c7cf86e0e804211f2f5b9785cab7d9254eaf8be9
Scott Gilbert <scottvtgilbert@gmail.com>
2024-02-20 21:29:21 -0500

    Updating favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 //Layout
 import Primary_Layout from "@/components/primary_layout.js";
+//Modules
+import Head from "next/head";
 //Styles
 import '../styles/reset_styles.scss';
 
@@ -7,9 +9,15 @@ import '../styles/reset_styles.scss';
 export default function App({Component, pageProps}) {
 
     return (
+        <>
+        <Head>
+            <link rel="icon" href="/bearpaw.png"/>
+            <title>Bear Cobble HQ</title>
+        </Head>
         <Primary_Layout >
             <Component {...pageProps} />
         </Primary_Layout>
+        </>
     )
 
 }
