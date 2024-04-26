165f011153e48b404b2050e41dc9315e05e3b2bf
Scott Gilbert <scottvtgilbert@gmail.com>
2024-04-26 08:34:18 -0400

    Payload too large in vercel deploymet debugging

--- src/pages/api/flow_calculation.js
+++ src/pages/api/flow_calculation.js
@@ -7,9 +7,9 @@ import sharp from 'sharp'
 
 export const config = {
     api: {
-      bodyParser: false
+        bodyParser: false
     }
-  }
+}
 
 export default async function (req, res) {
     //POST request.
@@ -17,10 +17,10 @@ export default async function (req, res) {
     //  Use multer middleware to create a single req.file property with the name of 'image' (the name of the field in the form handling the image upload) containing the information of the uploaded file
     if (req.method === 'POST') {
         upload.single('image')(req, res, async function (err) {
-            if(err){
+            if (err) {
                 console.log('There was an error in the multer package')
                 console.error(err)
-                res.status(500).json({error: 'Error occurred in image upload', message: err.message})
+                res.status(500).json({ error: 'Error occurred in image upload', message: err.message })
             }
             try {
                 console.log('Multer upload success', req.file.name)
@@ -36,6 +36,12 @@ export default async function (req, res) {
                 let resizedImageBuffer = inputImageHeight > inputImageWidth ? await sharpImage.resize(null, 1000).toBuffer() : await sharpImage.resize(1000).toBuffer()
                 // Base64 encode the image buffer for use by the roboflow API call
                 const base64ResizedImage = resizedImageBuffer.toString('base64');
+
+                /*Debugging*/
+                let sharpResizedImageInfo = await new sharp(resizedImageBuffer).metadata()
+                console.log(sharpResizedImageInfo)
+                /*End debugging*/
+
                 let response = await fetch(`https://detect.roboflow.com/ro-flow-sight-glasses/1?api_key=${process.env.ROBOFLOW_API_KEY}`, {
                     method: "POST",
                     headers: {
